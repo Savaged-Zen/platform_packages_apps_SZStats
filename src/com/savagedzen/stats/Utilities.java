f99c419169387a0b12e3b81d60fa5d56456f4aaa
bbedward <bbedward@gmail.com>
2011-05-09 17:22:15 -0500

    brandons first attempt to code android

--- src/com/savagedzen/stats/Utilities.java
+++ src/com/savagedzen/stats/Utilities.java
@@ -88,36 +88,28 @@ public class Utilities {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("/proc/version"), 256);
             try {
+                int position = 0;
                 procVersionStr = reader.readLine();
+                procVersionStr = procVersionStr.replaceFirst("Linux", "");
+                procVersionStr = procVersionStr.replaceFirst("version", "");
+                procVersionStr = procVersionStr.trim();
+                  for (int i=0; i < procVersionStr.length(); i++)
+		       if (procVersionStr.charAt(i) == ' ')
+		           position = i;
+
+	                   char[] newstring = new char[position];
+
+                  for (int j = 0; j < position; j++)
+		      newstring[j] = procVersionStr.charAt(j);
+
+               procVersionStr = new String(newstring);
+
             } finally {
                 reader.close();
             }
 
-            final String PROC_VERSION_REGEX =
-                "\\w+\\s+" + /* ignore: Linux */
-                "\\w+\\s+" + /* ignore: version */
-                "([^\\s]+)\\s+" + /* group 1: 2.6.22-omap1 */
-                "\\w+\\s+" +  /* ignore: (xxxxxx@xxxxx.constant) */
-                "\\w+\\s+" +  /* ignore: (gcc ..) */
-                "\\w+\\s+" +  /* ignore: #26 */
-                "\\w+\\s+" +  /* ignore: PREEMPT (optional) */
-                "\\w+\\s+"; /* ignore: date */
+            return procVersionStr;
 
-            Pattern p = Pattern.compile(PROC_VERSION_REGEX);
-            Matcher m = p.matcher(procVersionStr);
-
-            if (!m.matches()) {
-                Log.e(TAG, "Regex did not match on /proc/version: " + procVersionStr);
-                return "Unavailable";
-            } else if (m.groupCount() < 4) {
-                Log.e(TAG, "Regex match on /proc/version only returned " + m.groupCount()
-                        + " groups");
-                return "Unavailable";
-            } else {
-                return (new StringBuilder(m.group(1)).append("\n").append(
-                        m.group(2)).append(" ").append(m.group(3)).append("\n")
-                        .append(m.group(4))).toString();
-            }
         } catch (IOException e) {
             Log.e(TAG,
                 "IO Exception when getting kernel version for Device Info screen",
