d43516c4ecce4c022b03f782f3fe380dc987ef6a
Joshua Seidel <jsseidel1@gmail.com>
2011-05-16 15:25:03 -0500

    update url for stats
    Change-Id: Iedb7b3771a8464e4552a10764eb0fb417203ec3a

--- src/com/savagedzen/stats/ReportingService.java
+++ src/com/savagedzen/stats/ReportingService.java
@@ -98,7 +98,7 @@ public class ReportingService extends Service {
         Log.d(TAG, "SERVICE: Carrier ID=" + deviceCarrierId);
 
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://stats.savaged-zen.com/submit");
+        HttpPost httppost = new HttpPost("http://stats.savaged-zen.info/submit");
         try {
             List<NameValuePair> kv = new ArrayList<NameValuePair>(5);
             kv.add(new BasicNameValuePair("device_hash", deviceId));
